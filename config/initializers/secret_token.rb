94af6b5f78991a377661b19d6bad106dfb093448
Vishal Jain <vkjrul3z@gmail.com>
2014-02-17 11:34:50 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tumblrclone::Application.config.secret_token = '9ec30c9e45b3e0ca04f08e34d6017019458303c77386eb133ab10bd75abb68192faa43b6d1a28e805477fc5fa7e2b74715760fb6d347e7e57aa3923230bbeab6'
