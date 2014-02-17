d3beae66f322be74b951caa7d7b86bcc88188c02
Vishal Jain <vkjrul3z@gmail.com>
2014-02-17 18:54:53 -0500

    Fix precompile error

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,8 @@ module Tumblrclone
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.initialize_on_precompile = false
+
     # Auto-generate test files
     config.generators do |g|
       g.test_framework :rspec,
