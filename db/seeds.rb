4fdfbff742bcef3332e19f4f545c0fa4e3a01a8b
Vishal Jain <vkjrul3z@gmail.com>
2014-02-21 13:32:43 -0500

    Updates seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,38 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
- # 50.times do |i|
- #   User.create(email: Faker::Internet.safe_email,
- #               password: "password",
- #               username: Faker::Internet.user_name)
- # end
-#
-# User.all.each do |user|
-#   user.posts.create(
-#     title: Faker::Address.street_address,
-#     body: Faker::Lorem.paragraph(sentence_count=10),
-# end
-#
+5.times do |i|
+   User.create(email: Faker::Internet.safe_email,
+               password: "password",
+               username: Faker::Internet.user_name)
+end
+
+User.all.each do |user|
+  user.posts.create(
+    title: Faker::Address.street_address,
+    body: Faker::Lorem.paragraph(sentence_count = 10) )
+end
+
+
+u = User.create(email: "demo1@tumblr.com", password: "password", username: "DemoUser1")
+u2 = User.create(email: "demo2@tumblr.com", password: "password", username: "DemoUser2")
+u3 = User.create(email: "demo3@tumblr.com", password: "password", username: "DemoUser3")
+u4 = User.create(email: "demo4@tumblr.com", password: "password", username: "DemoUser4")
+
+u.posts.create(title: "My first post", body: "I am DemoUser1 and I wrote this. Feels good")
+u.posts.create(title: "My second post", body: "I am DemoUser1 and I wrote this. This is my second post")
+u.posts.create(title: "My third post", body: "I am DemoUser1 and I wrote this. This is my third post")
+u2.posts.create(title: "My first post", body: "I am DemoUser2 and I wrote this. Feels even better")
+u3.posts.create(title: "My first post", body: "I am DemoUser3 and I wrote this. Trying out this whole tumblr thing")
+u3.posts.create(title: "My second post", body: "I am DemoUser3 and I wrote this. This is my second post")
+u4.posts.create(title: "My first post", body: "I am DemoUser4 and I wrote this. Thanks for following me")
+
+
+Follow.create(user_id: u.id, follow_id: u2.id)
+Follow.create(user_id: u.id, follow_id: u3.id)
+Follow.create(user_id: u2.id, follow_id: u.id)
+
+
 # i = User.first.id
 #
 # while i < User.last.id do
