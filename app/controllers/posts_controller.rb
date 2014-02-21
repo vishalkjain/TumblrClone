57ec0cdb8c7d1c39b50eeaddff108d4a44009824
Vishal Jain <vkjrul3z@gmail.com>
2014-02-21 14:18:22 -0500

    Fixed bug where post could be edited to add tag that it already had

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,15 +8,9 @@ class PostsController < ApplicationController
     @post.user_id = current_user.id
 
     if params[:tag][:tag_name].length > 0
-     params[:tag][:tag_name].split(" ").each do |tagname|
-       @post.tags << Tag.find_or_create_by_tag_name(tagname)
-       # tag = Tag.find_by_tag_name(tagname)
-#        if tag
-#          @post.taggings.new(tag_id: tag.id)
-#        else
-#          @post.tags.new(tag_name: tagname)
-#        end
-     end
+      params[:tag][:tag_name].split(" ").each do |tagname|
+        @post.tags << Tag.find_or_create_by_tag_name(tagname)
+      end
     end
 
     #@post.tags.new(params[:tag])
@@ -25,19 +19,6 @@ class PostsController < ApplicationController
     else
       render :json => @post.errors.full_messages
     end
-    # github: gameboyvj
-#     site to see my repositories is just: https://github.com/gameboyvj?tab=repositories
-#     best projects are Chess, Asteroids because they are complete with graphics
-#     most of the other projects were practice backend work
-#     my final project right now is a Tumblr clone
-#     replicates the ability to CRUD (stands for create, read, update and delete)
-#     I CRUD posts, followers and tags
-#     posts = ability to make new blog post
-#     followers = ability for blog to follow and be followed
-#     tags = tag a post to organize topics (can click on tag to see all other posts in world that have same tag)
-#
-#     if u wanna try out asteroids, go to asteroids repo, click "download zip" on right bar, extract zip, and double click on index.html
-#       it should open in browser, use arrow keys to move and spacebar to shoot
   end
 
   def show
@@ -59,7 +40,9 @@ class PostsController < ApplicationController
       params[:tag][:tag_name].split(" ").each do |tagname|
         tag = Tag.find_by_tag_name(tagname)
         if tag
-          @post.taggings.new({tag_id: tag.id})
+          unless @post.tags.include?(tag)
+            @post.taggings.new({tag_id: tag.id})
+          end
         else
           @post.tags.new({tag_name: tagname})
         end
