b3340cfc8a58da87d4b64de4efc4534d7b1fc292
Vishal Jain <vkjrul3z@gmail.com>
2014-02-24 18:01:03 -0500

    Adds some styling to search page

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,6 +2,6 @@ class SearchesController < ApplicationController
   def index
     @results = PgSearch.multisearch(params[:search])
     @results = @results.includes(:searchable)
-
+    @search_name = params[:search]
   end
 end
