beb89b872f2f3fc31915f1b6a5ff34fff597acc9
Vishal Jain <vkjrul3z@gmail.com>
2014-02-25 13:37:39 -0500

    Fix styling on search page

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,7 @@
 class SearchesController < ApplicationController
   def index
     @results = PgSearch.multisearch(params[:search])
-    @results = @results.includes(:searchable).page(params[:page])
+    @results = @results.includes(:searchable).page(params[:page]).per(10)
     @search_name = params[:search]
 
   end
